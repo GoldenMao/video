0677bdeb0ead0090c8b988131704ec2d1d78384d
GoldenMao <zhangli568101@163.com>
2020-07-02 19:18:43 +0800

    readme and editorconfig

---
+++ frontend/dist/static/js/chunk-2d231006.ef3b429c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d231006"],{ef4c:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h2",[e._v("关于")]),t("div",[e._v(" 关于内容: ")])])}],u={name:"About"},a=u,i=t("2877"),s=Object(i["a"])(a,c,r,!1,null,"25bc5923",null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d231006.ef3b429c.js.map
