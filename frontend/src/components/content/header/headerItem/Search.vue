c449cda9f9a0118134714d356a1c93c590da732f
GoldenMao <zhangli568101@163.com>
2020-08-12 23:17:22 +0800

    实现搜索历史记录的显示，增加，删除和localStorage保存

--- frontend/src/components/content/header/headerItem/Search.vue
+++ frontend/src/components/content/header/headerItem/Search.vue
@@ -2,20 +2,17 @@
 
   <div class="searchout">
     <div class="search" :class="{changeShadow:isTrue}">
-      <input type="text" placeholder="请输入关键词搜索..." @focus="inpFocus" @blur="inpBlur"/>
-      <div class="search-inner">
+      <input type="text" placeholder="快速搜索..." ref="inputDom" @focus="getRecord">
+      <div class="search-inner" @click="setRecord">
         <img src="~assets/img/search.png" alt="search"/>
         <span>搜索</span>
       </div>
     </div>
-
-    <ul v-if="isTrue" class="history">
-      <li><a>天下无贼</a></li>
-      <li><a>叶问</a></li>
-      <li><a>当幸福来敲门</a></li>
-      <li><a>大象席地而坐</a></li>
-      <li><a>征途2</a></li>
-      <li><a>血战钢锯岭</a></li>
+    <ul v-show="isshow && recordKeywords.length" class="history">
+      <li v-for="(word, index) in recordKeywords" :key="index">
+        <a>{{word |forShort}}</a>
+      </li>
+      <li @click="deleteRecord" class="deleteRecord">删除记录</li>
     </ul>
   </div>
 
@@ -26,15 +23,51 @@
     name: "Search",
     data() {
       return {
-        isTrue: false,
+        msg: '',
+        recordKeywords: [],
+        isshow: false
       }
     },
     methods: {
-      inpFocus() {
-        this.isTrue = true
+      getRecord() { /*从localStorage中读取以前保存的历史记录，存入recordKeywords，并显示记录*/
+        let record = window.localStorage.getItem('searchRecord')
+        if (record) {
+          this.recordKeywords = JSON.parse(record).keywords
+          this.isshow = true // 显示记录
+        }
       },
-      inpBlur() {
-        this.isTrue = false
+      setRecord() {
+        /* 保存用户输入的搜索记录*/
+        this.msg = this.$refs.inputDom.value.trim()
+        if (this.msg) { // 搜索重复记录时，删除旧记录，将新记录添加到头部
+          if (this.recordKeywords.indexOf(this.msg)) {
+            this.recordKeywords.unshift(this.msg) // 添加到头部
+            let temp = new Set(this.recordKeywords) // 用Set去重后重新转为数组
+            this.recordKeywords = Array.from(temp)
+          }
+        }
+        // 设置保存记录数量的上限，超出后删除之前记录
+        if (this.recordKeywords.length > 6) {
+          this.recordKeywords.pop()
+        }
+        window.localStorage.setItem('searchRecord', JSON.stringify({keywords: this.recordKeywords}))
+        this.isshow = false // 隐藏记录
+      },
+      deleteRecord() {
+        /*删除，清空历史搜索记录*/
+        this.recordKeywords = []
+        window.localStorage.removeItem('searchRecord')
+        // window.localStorage.clear()
+      }
+    },
+    filters: {
+      /*对输入过长文本进行截取*/
+      forShort(item) {
+        if (item.length > 12) {
+          return item.slice(0, 12) + "..."
+        } else {
+          return item
+        }
       }
     }
   }
@@ -64,12 +97,11 @@
   li {
     margin: 5px 0 0 20px;
     width: 140px;
-
-
-
-
   }
-
+  .deleteRecord:hover{
+    color: rgb(255, 161, 36);
+    cursor:pointer;
+  }
   .search {
     height: 50px;
     width: 500px;
